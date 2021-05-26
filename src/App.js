03dea0bbb5c34f82cbbc4ed1378b8ef406c5a502
Grégory Paul <me@gpaul.me>
2021-05-26 08:55:43 +0200

    Update CSS for mobile

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
 
     return (
         <div className="App">
-            <h1 className="mainTitle">Reminder</h1>
+            <h1 className="MainTitle">Reminder</h1>
             <Form
                 language={language}
                 onLanguageChange={onLanguageChange}
